054b0e4256c22321c9083a4e9740c6f3019ac0c9
Joshua Bagley <JoshBagley@gmail.com>
2018-10-27 15:45:43 -0400

    Added New Block Breaker

---
+++ New Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    //Configuration Parameters
+
+    [SerializeField] Paddle paddle1;
+    [SerializeField] Vector2 launchVelocity = new Vector2(2f, 15f);
+
+    //state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = launchVelocity;
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
