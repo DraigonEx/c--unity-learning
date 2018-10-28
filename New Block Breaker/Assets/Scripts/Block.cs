e274138bf022ee4dde119cdbe22d3009760d070a
Joshua Bagley <JoshBagley@gmail.com>
2018-10-28 13:44:08 -0400

    Count Breakable Blocks

--- New Block Breaker/Assets/Scripts/Block.cs
+++ New Block Breaker/Assets/Scripts/Block.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 
 public class Block : MonoBehaviour {
 
+    [SerializeField] AudioClip breakSound;
+
+    // cached reference
+    LevelManager level;
+
+    public void Start()
+    {
+        level = FindObjectOfType<LevelManager>();
+        level.CountBreakableBlocks();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
+        level.RemoveBreakableBlock();
     }
 
 }
---
+++ New Block Breaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    [SerializeField] int breakableBlocks;
+
+    public void CountBreakableBlocks()
+    {
+        breakableBlocks++;
+    }
+
+    internal void RemoveBreakableBlock()
+    {
+        breakableBlocks--;
+    }
+}
