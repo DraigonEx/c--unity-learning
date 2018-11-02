ad9111af9466e7d717d16482cec023cdcbc2fbdd
Joshua Bagley <JoshBagley@gmail.com>
2018-11-02 17:32:01 -0400

    Updating Laser Defender to updated course

---
+++ Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        var newXPos = transform.position.x + deltaX;
+        var deltaY = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        var newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
